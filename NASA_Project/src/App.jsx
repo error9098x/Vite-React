da06882e743ad6af5e14b36a5ab920fb740019c1
ProCodec <43810146+error9098x@users.noreply.github.com>
2023-10-07 07:03:23 +0000

    vite setup

---
+++ NASA_Project/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className="w-screen h-screen bg-gray-100">
+        
+
+      </div>
+  )
+}
+
+export default App
