ee996542f4e949eb9380acf1b12e9eb5b96ba73e
ProCodec <43810146+error9098x@users.noreply.github.com>
2023-10-07 07:04:42 +0000

    vite setup

--- NASA_Project/src/App.jsx
+++ NASA_Project/src/App.jsx
@@ -1,10 +1,10 @@
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-      <div className="w-screen h-screen bg-gray-100">
-        
+      <div className="w-screen h-screen bg-red-300">
+
 
       </div>
   )
